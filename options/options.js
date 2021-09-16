96c6f355af642b8c09954b47b25443b4289141f5
Crowdbotics <team@crowdbotics.com>
2021-09-16 22:16:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "billowing_star_30625Identifier",
+  url: "https://billowing_star_30625Identifier.botics.co",
+  api: "https://billowing_star_30625Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
